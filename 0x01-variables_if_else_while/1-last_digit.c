1897c3b719323589edcc41644c1995a9b0d951db
perried <periedamas203@gmail.com>
2022-09-09 16:16:52 -0700

    fixing betty style errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	else if (last_digit < 6)
 	{
 		printf("Last digit of %d is %d", n, last_digit);
-		printf("and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n");
 	}
 	else if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
