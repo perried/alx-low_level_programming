f85ad9f59283e0b8254fb4edfb21c56aa4edd5ce
perried <periedamas203@gmail.com>
2022-09-08 18:11:56 -0700

    last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
