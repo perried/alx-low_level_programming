d53432200457b800751a76e98ace458a9c16add0
perried <periedamas203@gmail.com>
2022-09-12 16:14:45 -0700

    done with task 0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch = 122;
+
+	while (ch >= 97)
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
