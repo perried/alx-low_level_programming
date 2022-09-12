e2b484c20732d80c7c974b5ff9e72de2b48f3fc4
perried <periedamas203@gmail.com>
2022-09-12 16:07:01 -0700

    done with task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
