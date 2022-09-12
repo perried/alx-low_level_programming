b0d441af5827634cd360a96aa2a9e6efda418bda
perried <periedamas203@gmail.com>
2022-09-12 13:07:40 -0700

    done with task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch = 65;
+	
+	while (ch <= 90)
+	{
+		putchar(ch);
+	}
+
+	ch = 97;
+
+	while (ch <= 122)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
