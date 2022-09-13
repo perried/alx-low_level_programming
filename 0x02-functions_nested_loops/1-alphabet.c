a92716f0952002b33593d23cbdf75ea81069ebdd
perried <periedamas203@gmail.com>
2022-09-13 10:43:45 -0700

    done with task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - printing alphabet
+ *
+ * return: On success 0
+ */
+int printAlphabet(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
+int main(void)
+{
+	printAlphabet();
+	return (0);
+}
