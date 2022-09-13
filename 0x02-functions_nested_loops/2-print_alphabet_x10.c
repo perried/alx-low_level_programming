7f660f06cefd407f4226a3dff7f6a9667dcc4d7c
perried <periedamas203@gmail.com>
2022-09-13 11:10:47 -0700

    done with task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: On success 0
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
