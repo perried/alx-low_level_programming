e3423b9cb522c58d71ac1a5861351a65a6d4f3e5
perried <periedamas203@gmail.com>
2022-09-13 10:26:05 -0700

    betty style fixing

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,6 @@
-#include <unistd.h>
+#ifndef _PUTCHAR
+#define _PUTCHAR
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- *Return: On success 1.
- *On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+
+#endif
