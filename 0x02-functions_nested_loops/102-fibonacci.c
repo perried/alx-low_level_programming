7188dfb4b53d459619be9e12e5cebe2bb55cb36b
perried <periedamas203@gmail.com>
2022-09-13 19:39:41 -0700

    done with task 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,15 +9,16 @@ int main(void)
 {
 	int t1 = 1;
 	int t2 = 2;
-	int next_term = t1 + t2;
+	int t3;
 	int i;
 
+	printf("%d, %d", t1, t2);
 	for (i = 3; i <= 50; ++i)
 	{
-		printf("%d, %d", t1, t2);
-		t1 = next_term;
-		t2 += next_term;
-		next_term = t1 + t2;
+		t3 = t1 + t2;
+		printf(", %d", t3);
+		t1 = t2;
+		t2 = t3;
 	}
 	printf("\n");
 	return (0);
