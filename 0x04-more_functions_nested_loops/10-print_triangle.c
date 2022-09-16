351ec23872d9ff0d90565ded8ab1f673cce84a4d
perried <periedamas203@gmail.com>
2022-09-16 11:14:24 -0700

    done with task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *print_triangle - prints a triangle, followed by a new line
+ *@size: base and hieght of a triangle
+ *Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int height;
+		int spaces;
+		int counter = 1;
+		int builder;
+
+		for (height = 1; height <= size; height++)
+		{
+			spaces = size - counter;
+			while (spces >= 1)
+			{
+				_putchar(' ');
+				spaces--;
+			}
+			builder = counter;
+			while (builder >= 1)
+			{
+				_putchar('#');
+				builder--;
+			}
+			_putchar('\n');
+			counter++;
+		}
+	}
+}
