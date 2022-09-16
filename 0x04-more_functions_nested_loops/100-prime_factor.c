d0ef3be956e035fa722c81a6e57ff098a3847eac
perried <periedamas203@gmail.com>
2022-09-16 14:57:05 -0700

    done with task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-    long int x = 612852475143;
-    long int py;
+	long int x = 612852475143;
+	long int py;
 
-    for (py = 2; py < x; py++)
-	  {
-		    if (x % py == 0)
-		    {
-			      x = x / py;
-		    }
-	  }
-	printf("%ld\n", py);
+	for (py = 2; j < x; py++)
+	{
+		if (x % j == 0)
+		{
+			x = x / py;
+		}
+	}
+	printf("%ld\n", j);
 	return (0);
 }
