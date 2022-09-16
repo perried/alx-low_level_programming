4aac728490e99faf9b1add9a68cffb05bfb8bad9
perried <periedamas203@gmail.com>
2022-09-16 09:35:23 -0700

    done with task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.c"
+
+/**
+ * print_square - prints a suare, followed by a new line
+ * @size: size of the square
+ * Return: On success 0
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int n;
+
+		for (i = 0; i <= size; i++)
+		{
+			n = 0;
+			while (n <= size)
+			{
+				_putchar('#');
+				n++;
+			}
+		}
+		_putchar('\n');
+	}
+}
