a55e5afd0387fd7c004567006000f73049126ebb
perried <periedamas203@gmail.com>
2022-09-29 14:54:13 -0700

    a fuction that calculate exponentiation

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the base number
+ * @y: the exponent
+ * Return: -1 if y is lower than 0 and power of x raised to y if otherwise
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
