2c51b816d4bdfc605f52df448b13919d26e0ec1a
perried <periedamas203@gmail.com>
2022-09-29 18:19:39 -0700

    fixing errors

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
+	int lenstr = 0;
+
 	if (*s)
 	{
-		return (1 + _strlen_recursion(s + 1));
+		lenstr++;
+		lenstr += _strlen_recursion(s + 1);
 	}
+	return (lenstr);
 }
