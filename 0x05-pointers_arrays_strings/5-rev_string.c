17e13f3be1a213061c4d40935ad61023d6532b29
perried <periedamas203@gmail.com>
2022-10-09 07:56:09 -0700

    a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: a string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = _strlen(s) - 1;
+	char *p = &s;
+	int n = 0;
+
+	while (len >= 0)
+	{
+		s[n] = p[len];
+		--len;
+		++n;
+	}
+}
