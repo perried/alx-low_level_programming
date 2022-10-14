a71166db2338a474d68e6d61c109519be2a44418
perried <periedamas203@gmail.com>
2022-10-13 18:54:20 -0700

    a program that prints element of a string in step of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: a string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
