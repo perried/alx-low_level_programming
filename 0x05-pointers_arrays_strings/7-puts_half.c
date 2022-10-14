4c429b6576c9f7eaa990b6e14215a35a7d53f919
perrie-d <111010793+perried@users.noreply.github.com>
2022-10-14 02:38:44 +0000

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,11 @@
 void puts_half(char *str)
 {
 	int n;
+	int i = 0;
 
-	n = _strlen(str) / 2;
-	while (str[n] != '\0')
+	if (_strlen(str) % 2 != 0)
+		i += 1;
+	for (n = (_strlen(str) + i) / 2; n < _strlen(str); n++) 
 	{
 		_putchar(str[n]);
 		n++;
