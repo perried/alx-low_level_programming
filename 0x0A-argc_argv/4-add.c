c165cc79bc6c53abf530549731e8d6f00e14f52b
perrie_d <111010793+perried@users.noreply.github.com>
2023-07-24 14:27:36 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i j;
 	int sum = 0;
 
 	if (argc == 1)
@@ -20,15 +20,12 @@ int main(int argc, char *argv[])
 	}
 	else
 		for (i = 1; i < argc; i++)
-			if (!isdigit(argv[i]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-	   else
-			{
-				sum += atoi(argv[i]);
-			}
+    for (j = 0; argv[i][j] != '\0'; j++)
+			    if (!isdigit(argv[i][j]))
+			    {
+				    printf("Error\n");
+				    return (1);
+			sum += atoi(argv[i]);
 	 printf("%d\n", sum);
 
 	return (0);
