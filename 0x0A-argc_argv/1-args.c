deac39eb9e586c0568e43a2c0e4b84d7f8609781
perrie <periedamas203@gmail.com>
2023-07-24 03:27:15 -0700

    Done with task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the number of arguments
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: Exit status
+ */
+
+int main(int argc, char **argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
