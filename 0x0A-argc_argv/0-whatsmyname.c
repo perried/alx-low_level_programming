a8f7d37450ed0eb3dfcd64eec5e6c025924e14b2
perrie <periedamas203@gmail.com>
2023-07-24 03:05:42 -0700

    Done with task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - The main function of the program
+ * @argc - the number of args passed to the command line including program name
+ * @argv - the array of args passed to the command line including programname
+ * Return - Exit Status of the program after execution
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
