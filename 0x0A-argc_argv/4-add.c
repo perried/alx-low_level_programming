023dfff46d5b8cfd9a283e9fb45e9c33f50aeac7
perrie <periedamas203@gmail.com>
2023-07-24 05:41:03 -0700

    Done with task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,14 +19,21 @@ int main(int argc, char *argv[])
 		printf("%d\n", sum);
 	}
 	else
+	{
 		for (i = 1; i < argc; i++)
-    for (j = 0; argv[i][j] != '\0'; j++)
-			    if (!isdigit(argv[i][j]))
-			    {
-				    printf("Error\n");
-				    return (1);
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 			sum += atoi(argv[i]);
-	 printf("%d\n", sum);
+		}
+	}
+	printf("%d\n", sum);
 
 	return (0);
 }
