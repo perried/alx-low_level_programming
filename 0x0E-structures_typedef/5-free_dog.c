c39e3ad580ad151ff8e78602592ef861e914b703
perrie <perriedamas203@gmail.com>
2023-11-15 06:49:04 -0800

    Done with task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees the memory allocated to struct dog
+ * @d: A struct to free memory
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+	        free(d->owner);
+		free(d);
+	}
+}
