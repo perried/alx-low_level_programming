ff5ff225ba7f6a8d6a93ae293a55f62a745b5fdb
PERRIE <111010793+perried@users.noreply.github.com>
2023-11-15 16:23:58 +0200

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = "(nil)";
+	/*if (d->age == NULL)
+		d->age = "(nil)";*/
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
